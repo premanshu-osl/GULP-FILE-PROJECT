2fcfde707ef9d363bcad309f00220edce1b23ed5
Premanshu Pareek <premanshu.p@opensenselabs.com>
2019-10-10 17:01:04 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Gulpfile Boilerplate
+
+#### Stpes to use
+
+1. Run command 'npm install'
+2. Run command 'gulp'
+
+#### Steable Version
+
+1. Node - 12.9.1
+2. NPM - 6.9.0
+2. Gulp 
+  * CLI Version - 2.2.0
+  * Version - 4.0.2
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const { src, dest, watch, series, parallel} = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const csscomb = require('gulp-csscomb');
+const sourcemaps = require('gulp-sourcemaps');
+const bless = require('gulp-bless');
+
+const files = {
+  scssPath: 'src/sass/**/*.scss'
+}
+
+const prefixerOptions = {
+  browsers: ['last 2 versions']
+};
+
+function scssTask() {
+  return src(files.scssPath)
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(autoprefixer(prefixerOptions))
+  .pipe(csscomb())
+  .pipe(sourcemaps.write('.'))
+  .pipe(dest('dist/css/')
+  .pipe(bless())
+  .pipe(dest('dist/splitCSS'))
+  );
+}
+
+function watchTask() {
+  watch([files.scssPath],
+    parallel(scssTask));
+}
+
+exports.default = series(
+  parallel(scssTask),
+  watchTask
+);
